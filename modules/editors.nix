732ba35c0c9ae339ef96971061edf6833d926cdf
Anthony M. Bonafide <AnthonyMBonafide@pm.me>
2025-11-04 08:35:28 -0500

    feat(nvim): Switch From Snacks Explorer To Mini Files

--- modules/editors.nix
+++ modules/editors.nix
@@ -132,6 +132,35 @@
         }
       '';
 
+      minifiles = ''
+        return {
+          "nvim-mini/mini.files",
+          opts = {
+            windows = {
+              preview = true,
+              width_focus = 30,
+              width_preview = 30,
+            },
+          },
+          keys = {
+            {
+              "<leader>fm",
+              function()
+                require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+              end,
+              desc = "Open mini.files (Directory of Current File)",
+            },
+            {
+              "<leader>fM",
+              function()
+                require("mini.files").open(vim.uv.cwd(), true)
+              end,
+              desc = "Open mini.files (cwd)",
+            },
+          },
+        }
+      '';
+
       neotest = ''
         return {
           "nvim-neotest/neotest",
