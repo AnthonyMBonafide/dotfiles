30db04e94d00e3f62e48ce33818ce9337bb1b9ac
Anthony M. Bonafide <AnthonyMBonafide@pm.me>
2025-11-03 22:37:32 -0500

    fix(nvim): Fix Treesitter Not Being Installed
    Install Treesitter via home-manager alongside NVIM

--- modules/editors.nix
+++ modules/editors.nix
@@ -15,15 +15,63 @@
 
   # LazyVim Configuration
   programs.lazyvim = {
-    enable = true;
+  enable = true;
+  installCoreDependencies = true;
+  config = {
+      options = ''
+        vim.opt.swapfile = false
+      '';
+
+      keymaps = ''
+        vim.keymap.set("i", "jk", "<Esc>")
+      '';
+    };
+
+    plugins = {
+      colorscheme = ''
+        return {
+          "catppuccin/nvim",
+          opts = { flavour = "mocha" },
+        }
+      '';
+    };
+    
+  extras = {
+    lang.nix.enable = true;
+    test.core = {
+        enable = true;
+    };
+
+    lang.rust = {
+      enable = true;
+      installDependencies = true;
+      installRuntimeDependencies = true;
+    };
+    lang.go = {
+      enable = true;
+      installDependencies = true;        # Install gopls, gofumpt, etc.
+      installRuntimeDependencies = true; # Install go compiler
+    };
   };
 
-  # Set Neovim as default editor
-  programs.neovim = {
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-  };
+  # Additional packages (optional)
+  extraPackages = with pkgs; [
+    tree-sitter  # Tree-sitter CLI
+    nixd         # Nix LSP
+    alejandra    # Nix formatter
+  ];
+
+  # Only needed for languages not covered by LazyVim
+  treesitterParsers = with pkgs.vimPlugins.nvim-treesitter.grammarPlugins; [
+    templ     # Go templ files
+  ];
+};
+  # # Set Neovim as default editor
+  # programs.neovim = {
+  #   defaultEditor = true;
+  #   viAlias = true;
+  #   vimAlias = true;
+  # };
 
   # Helix Editor Configuration
   programs.helix = {
