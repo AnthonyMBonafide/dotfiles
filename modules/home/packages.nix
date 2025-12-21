1d8b6f3e75575ba1a57b1e8ffe8ff90ac5ffea5b
Anthony M. Bonafide <AnthonyMBonafide@pm.me>
2025-12-20 23:24:51 -0500

    feat(update): Update Flake
    Signed-off-by: Anthony M. Bonafide <AnthonyMBonafide@pm.me>

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -11,13 +11,53 @@ let
         --add-flags "--ozone-platform=wayland"
     '';
   };
+
+  spotify-fixed = pkgs.symlinkJoin {
+    name = "spotify";
+    paths = [ pkgs.spotify ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/spotify \
+        --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [
+          pkgs.libayatana-appindicator
+          pkgs.libdbusmenu
+          pkgs.gtk3
+          pkgs.pango
+          pkgs.harfbuzz
+          pkgs.atk
+          pkgs.cairo
+          pkgs.gdk-pixbuf
+          pkgs.glib
+          pkgs.alsa-lib
+          pkgs.gcc.cc.lib
+          pkgs.xorg.libX11
+          pkgs.xorg.libXcomposite
+          pkgs.xorg.libXdamage
+          pkgs.xorg.libXext
+          pkgs.xorg.libXfixes
+          pkgs.xorg.libXrandr
+          pkgs.xorg.libxcb
+          pkgs.libxkbcommon
+          pkgs.mesa
+          pkgs.libgbm
+          pkgs.expat
+          pkgs.systemd
+          pkgs.nspr
+          pkgs.nss
+          pkgs.dbus
+          pkgs.cups
+          pkgs.at-spi2-atk
+          pkgs.at-spi2-core
+        ]}"
+    '';
+  };
 in
 {
   # General packages and applications
   home.packages = with pkgs; [
     # Applications
     discord-wayland
-    spotify
+    spotify-fixed
 
     # Steam is now enabled at system level in configuration.nix with proper Wayland support
 
